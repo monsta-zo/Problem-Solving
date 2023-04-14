cb8aef06ca94fd015ef62c6319f5646d90a7ba49
Jo Jun Seo <monstazo911@gmail.com>
2023-04-14 12:37:37 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,16 +1,7 @@
 function solution(score) {
-    const map = new Map();
-    const result = [...score];
-    score.sort((a,b)=>{
-        return (b[0]+b[1])/2-(a[0]+a[1])/2
-    })
-    map.set(score[0],1);
-    for(let i=2; i<=score.length; i++){
-        if((score[i-1][0]+score[i-1][1])/2===(score[i-2][0]+score[i-2][1])/2)
-            map.set(score[i-1],map.get(score[i-2]));
-        else
-            map.set(score[i-1],i);
-    }
-    
-    return result.map(v=>map.get(v)); 
+    return score.map(el=>{
+        return (
+            score.filter((v)=>(v[0]+v[1])/2 > (el[0]+el[1])/2).length+1
+        );
+    });
 }
