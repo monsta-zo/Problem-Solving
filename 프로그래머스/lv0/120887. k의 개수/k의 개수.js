84cc84b512e5ae5d8b708f54f8844f6ec5dbf3cb
Jo Jun Seo <monstazo911@gmail.com>
2023-04-16 12:16:21 +0900

    [level 0] Title: k의 개수, Time: 9.68 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,10 +1,7 @@
 function solution(i, j, k) {
     let count = 0;
-    regexp = new RegExp(`${k}`,"g")
     for(let index=i; index<=j; index++){
-        const match = index.toString().match(regexp);
-        if(match)
-            count+=match.length;
+        count += index.toString().split(k).length-1;
     }
     return count;
 }
