87b999f66b2093bbe429e31875951fc54d400a7a
Jo Jun Seo <monstazo911@gmail.com>
2023-04-16 11:23:02 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -5,8 +5,7 @@ function solution(chicken) {
     let service = 0;
     while(coupon>=10){
         service += Math.floor(coupon / 10); // 서비스 치킨
-        let remain = coupon % 10;
-        coupon = remain + Math.floor(coupon / 10);
+        coupon = coupon%10 + Math.floor(coupon / 10);
     }
     return service;
 }
