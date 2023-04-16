c7e772bdb0db00d2cb3cadee08d2f58e12b2ee67
Jo Jun Seo <monstazo911@gmail.com>
2023-04-16 10:55:14 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,10 +1,5 @@
 function solution(id_pw, db) {
-    let result = "fail";
-    db.forEach(d=>{
-        if(JSON.stringify(id_pw) === JSON.stringify(d))
-            result = "login"
-        else if(id_pw[0]===d[0])
-            result = "wrong pw"
-    })
-    return result;
+    const [id,pw] = id_pw;
+    const map = new Map(db);
+    return map.has(id)?(map.get(id)===pw?"login":"wrong pw"):"fail";
 }
