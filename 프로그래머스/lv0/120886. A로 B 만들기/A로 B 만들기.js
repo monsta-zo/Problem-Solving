4f9a00cdc90229f8438e3635918a012431c2a8d9
Jo Jun Seo <monstazo911@gmail.com>
2023-04-16 11:55:09 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,3 +1,3 @@
 function solution(before, after) {
-    return JSON.stringify([...before].sort())===JSON.stringify([...after].sort())?1:0;
+    return [...before].sort().join()===[...after].sort().join()?1:0;
 }
