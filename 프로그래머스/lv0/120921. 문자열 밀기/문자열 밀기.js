ac38cf24a71473f5aae01328136b3db8098316a1
Jo Jun Seo <monstazo911@gmail.com>
2023-04-19 15:09:02 +0900

    [level 0] Title: 문자열 밀기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,11 @@
+function solution(A, B) {
+    let count = 0;
+    for(let i=0; i<A.length; i++){
+        if(A===B)
+            return count;
+        A = A.slice(-1) + A.slice(0,A.length-1);
+        count++;
+    }
+    return -1;
+    
+}
