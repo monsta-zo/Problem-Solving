bc0f51de87a024106920f10f985e690f050e0d49
Jo Jun Seo <monstazo911@gmail.com>
2023-04-21 17:15:24 +0900

    [level 1] Title: 체육복, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,40 +1,25 @@
 function solution(n, lost, reserve) {
-    const students = new Array(n).fill(0);
-    lost.forEach(student=>{
-        students[student-1] -= 1;
-    })
-    reserve.forEach(student=>{
-        students[student-1] += 1;
-    })
-    
-    students.forEach((student,i,arr)=>{
-        if(student===-1){
-            if(arr[i-1]===1&&arr[i+1]!==1){
-                arr[i]++;
-                arr[i-1]--;
-            }
-            if(arr[i-1]!==1&&arr[i+1]===1){
-                arr[i]++;
-                arr[i+1]--;
-            }
+    const students = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        students[i] = 1;
+    }
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
+
+    for(let i = 1; i <= n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+                students[i-1]++;
+                students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+                students[i+1]++;
+                students[i]--;
         }
-    })
-    students.forEach((student,i,arr)=>{
-        if(student===-1){
-            if(arr[i-1]===1&&arr[i+1]!==1){
-                arr[i]++;
-                arr[i-1]--;
-            }
-            if(arr[i-1]!==1&&arr[i+1]===1){
-                arr[i]++;
-                arr[i+1]--;
-            }
-            if(arr[i-1]===1&&arr[i+1]==1){
-                arr[i]++;
-            }
+    }
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
         }
-    })
-    
-    return n - students.filter(student => student===-1).length;
-    
+    }
+    return answer;
 }
