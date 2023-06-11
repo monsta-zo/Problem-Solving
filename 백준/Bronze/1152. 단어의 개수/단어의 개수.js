2ce4876e8d19bbd6b7c354a2a70671c09f8b75fb
Jo Jun Seo <monstazo911@gmail.com>
2023-06-11 09:43:46 +0900

    [Bronze II] Title: 단어의 개수, Time: 152 ms, Memory: 17420 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -2,5 +2,4 @@ let fs = require("fs");
 
 let input = fs.readFileSync("dev/stdin").toString().split("\n");
 
-if(input[0]===" ") console.log(0);
-else console.log(input[0].trim().split(" ").length);
+console.log(input[0]===" "?0:input[0].trim().split(" ").length)
