1612ee32eba9ed16384051f6add6cc94a0cdb755
Jo Jun Seo <monstazo911@gmail.com>
2023-07-08 23:11:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 464 ms, Memory: 54396 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,27 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
+
+const N = Number(input[0])
+
+const arr = [];
+for(let i=1; i<N+1; i++){
+  arr.push(input[i].split(" ").map(Number));
+}
+
+arr.sort((a,b)=> {
+  if(a[0]<b[0])
+    return -1;
+  else if(a[0]===b[0]){
+    return a[1]-b[1]
+  }
+  else
+    return 1;
+})
+
+let result = ""
+for(let i=0; i<N; i++){
+  result += `${arr[i][0]} ${arr[i][1]}\n`
+}
+console.log(result);
+
