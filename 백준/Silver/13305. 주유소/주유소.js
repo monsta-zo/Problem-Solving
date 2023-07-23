4f251eb8ac490a7f4d4e7392964bbd81531cabaf
Jo Jun Seo <monstazo911@gmail.com>
2023-07-23 17:31:24 +0900

    [Silver III] Title: 주유소, Time: 216 ms, Memory: 20236 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -7,21 +7,16 @@ let city = input[2].split(" ").map(Number);
 
 let result = 0;
 
-let minIdx = city.length+1;
-
-while(true){
-  let min = Math.min(...city.slice(0,city.length-1));
-  let minIdx = city.indexOf(min);
-
-  result += min * distance.reduce((acc,cur,idx)=>{
-    if(idx>=minIdx) return acc+cur;
-    else return acc;
-  },0);
-
-  if(minIdx===0) break;
-  
-  city = city.slice(0,minIdx+1);
-  distance = distance.slice(0,minIdx);
+for(let i=0; i<city.length-1; i++){
+  let sum = 0;
+  for(let j=i+1; j<city.length; j++){
+    sum += distance[j-1]
+    if(city[i]>=city[j]){
+      result+=city[i]*sum;
+      i=j-1;
+      break;
+    }
+  }
 }
 
 console.log(result);
