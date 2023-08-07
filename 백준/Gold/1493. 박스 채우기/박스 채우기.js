d8079b714c6fe8ed647d792ade14d916ecc9d90d
Jo Jun Seo <monstazo911@gmail.com>
2023-08-07 19:07:13 +0900

    [Gold III] Title: 박스 채우기, Time: 132 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Gold/1493. 박스 채우기/박스 채우기.js
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.js
@@ -2,39 +2,46 @@ let fs = require("fs");
 
 let input = fs.readFileSync("dev/stdin").toString().split("\n");
 
-function nearestSquare(x) {
-  let i = 1;
-  while((2**i)<= x) i += 1;
-  return i-1;
+const [length, width, height] = input[0].split(" ").map(Number);
+const cubes = new Array(20).fill(0);
+
+const N = Number(input[1]);
+
+for(let i=2; i<N+2; i++){
+  const [size, num] = input[i].split(" ").map(Number);
+
+  cubes[size] = num;
 }
 
-let [length, width, height] = input[0].split(" ").map(Number);
-let cubes = Array(20).fill(0);
+function nearestSquare(length){
+  let idx = 0;
+  while(2**idx <= length){
+    idx++;
+  }
 
-let n = Number(input[1]);
-for(let i=2; i<=n+1; i++){
-  let [a,b] = input[i].split(" ").map(Number);
-  cubes[a] = b;
+  return idx-1;
 }
 
-let size = 19;
-size = nearestSquare(length);
-size = Math.min(size, nearestSquare(width));
-size = Math.min(size, nearestSquare(height));
+let start = nearestSquare(length);
+start = Math.min(start, nearestSquare(width));
+start = Math.min(start, nearestSquare(height));
 
-let res = 0;
 let used = 0;
+let res = 0;
 
-for(let i=size; i>=0; i--){
+for(let i = start; i>=0; i--){
   used *= 8;
-  cur = (2**i);
 
-  let required = parseInt(length/cur) * parseInt(width/cur) * parseInt(height/cur) - used;
+  let edge = 2**i;
 
-  let usage = Math.min(required, cubes[i]);
-  res += usage;
+  let usage = Math.min(cubes[i], parseInt(length/edge)*parseInt(width/edge)*parseInt(height/edge)-used);
+
   used += usage;
+
+  res += usage;
 }
 
-if(used == length * width * height) console.log(res);
-else console.log(-1);
+
+if(used === length * width * height) console.log(res);
+else console.log(-1)
+
