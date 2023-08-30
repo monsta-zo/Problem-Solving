02daded901e57861ae88cc7f23bdcbac7c52510e
Jo Jun Seo <monstazo911@gmail.com>
2023-08-30 13:50:56 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,4 +1,17 @@
 function solution(keymap, targets) {
-    var answer = [];
+    const map = new Map();
+    const answer = [];
+    
+    keymap.forEach(key=>{
+        for(let i=0; i<key.length; i++){
+            map.set(key[i], Math.min(i+1,map.get(key[i])||i+1));
+        }
+    })
+    
+    for(const item of targets){
+        answer.push(item.split("").reduce((acc,cur)=>acc+map.get(cur),0) || -1); 
+    }
+    
     return answer;
+    
 }
