a01d534f218f878e6f0498a9e49375da39dea743
Jo Jun Seo <monstazo911@gmail.com>
2023-08-30 15:25:30 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -2,22 +2,16 @@ function solution(id_list, report, k) {
     report = [...new Set(report)]
 
     const reporter = new Map();
-    const answer = new Map();
+    const times = new Map();
     
     report.forEach((r, index)=>{
         const [user, badUser] = r.split(" ");
-        if(!reporter.get(badUser)) reporter.set(badUser, [user]);
-        else reporter.set(badUser, [user,...reporter.get(badUser)]);
-        
+        times.set(badUser, (times.get(badUser)||0)+1);
+        if(!reporter.get(user)) reporter.set(user, [badUser]);
+        else reporter.set(user, [badUser,...reporter.get(user)]);
     })
     
-    for(const key of reporter.keys()){
-        if(reporter.get(key).length>=k){
-            reporter.get(key).forEach(r=>{
-                answer.set(r,(answer.get(r)||0)+1);
-            })
-        }
-    }
-    
-    return id_list.map((id)=>answer.get(id)||0)
+    return id_list.map((id)=>{
+        return reporter.get(id)?reporter.get(id).filter((badUser=>times.get(badUser)>=k)).length:0
+    })
 }
