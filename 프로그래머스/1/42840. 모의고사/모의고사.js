6cc8920bdb567037785ded89ce58cc9a4cc15627
Jo Jun Seo <monstazo911@gmail.com>
2023-11-21 19:59:42 +0900

    [level 1] Title: 모의고사, Time: 3.25 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -16,17 +16,11 @@ function solution(answers) {
         if(answers[i] === two[i%8]) score[1]++;
         if(answers[i] === three[i%10]) score[2]++;
     }
-    let max = -1;
-    for(let i=0; i<3; i++){
-        if(score[i] > max){
-            max = score[i];
-            answer = [i+1];
-        }
-        else if (score[i] === max){
-            answer.push(i+1);
-        } 
-    }
+    let max = Math.max(score[0], score[1], score[2]);
     
+    for(let i=0; i<3; i++){
+        if(max===score[i]) answer.push(i+1);
+    }
     return answer;
     
 }
