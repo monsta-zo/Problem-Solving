77e62b2c97e57b9d975f1825884d01fc503ed67a
Jo Jun Seo <monstazo911@gmail.com>
2023-11-21 21:24:54 +0900

    [level 2] Title: 전화번호 목록, Time: 446.23 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -3,20 +3,15 @@
 
 // 해시 테이블을 어떻게 이용할 수 있을까?
 function solution(phone_book) {
-    phone_book.sort((a,b)=>a.length-b.length);
-    
     const map = new Map();
     let flag = true;
     
-    phone_book.forEach((phone)=>{
-        for(let i=0; i<phone.length; i++){
-            const sub = phone.substring(0,i+1);
-            if(map.has(sub) && map.get(sub) === i+1){
-                flag = false;
-            }
-            else {
-                map.set(sub, phone.length);
-            }
+    phone_book.forEach(phone=>map.set(phone));
+    
+    phone_book.forEach(phone=>{
+        for(let i=0; i<phone.length-1; i++){
+            if(map.has(phone.substring(0,i+1)))
+                flag=false;
         }
     })
     return flag;
