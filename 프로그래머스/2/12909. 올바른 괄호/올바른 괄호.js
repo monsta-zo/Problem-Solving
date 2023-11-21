2d4981696744bbf094ea21a3d94f00fefc216cb8
Jo Jun Seo <monstazo911@gmail.com>
2023-11-21 22:27:52 +0900

    [level 2] Title: 올바른 괄호, Time: 5.14 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,9 @@
-// 왜 시간초과가 날까? 현재 O(N) 인데 이보다 더 단축할 수 있을까?
-
 function solution(s){
     let count = 0;
-   for(let char of s){
-       if(char === "(") count++;
-       else count --;
-       if(count < 0) return false;
-   }
-    if(count > 0) return false;
-    return true;
+    for(let char of s){
+        if(char === "(") count++;
+        else count --;
+        if(count < 0) return false;
+    }
+    return count === 0;
 }
