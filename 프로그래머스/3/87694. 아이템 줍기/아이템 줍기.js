6865e99dff2aa6210946f4d2cd04b5778f051387
Jo Jun Seo <monstazo911@gmail.com>
2023-11-23 20:56:21 +0900

    [level 3] Title: 아이템 줍기, Time: 0.55 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
@@ -0,0 +1,40 @@
+// 직사각형 안쪽을 2, 테두리를 1로 채운다.
+
+const STEP = [[0,1],[0,-1],[1,0],[-1,0]];
+
+function solution(rectangle, characterX, characterY, itemX, itemY) {
+    const map = Array.from({length:103}, ()=>Array(103).fill(0));
+    characterX *= 2;
+    characterY *= 2;
+    itemX *= 2;
+    itemY *= 2;
+    
+    rectangle.map(([leftX,leftY,rightX,rightY])=>{
+        for(let i=leftX*2; i<=rightX*2; i++){
+            for(let j=leftY*2; j<=rightY*2; j++){
+                if(i===leftX*2 || i===rightX*2 || j===leftY*2 || j===rightY*2){
+                    if(map[i][j]===0) map[i][j] = 1;
+                }
+                else map[i][j] = 2;
+            }
+        }        
+    })
+    
+    let queue = [[characterX,characterY,0]];
+    
+    while(queue.length){
+        let [x,y,count] = queue.shift();
+        map[x][y] = 0;
+        
+        if(x===itemX && y===itemY) return count/2;
+        
+        for(let i=0; i<4; i++){
+            let nextX = x+STEP[i][0];
+            let nextY = y+STEP[i][1];
+            
+            if(map[nextX][nextY]!==1) continue;
+            queue.push([nextX,nextY,count+1]);
+        }
+    }
+    return 0;
+}
