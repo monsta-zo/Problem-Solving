60261330a1b80064924060eb992873c9fcaac416
Jo Jun Seo <monstazo911@gmail.com>
2024-03-11 17:29:44 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,21 +1,4 @@
 function solution(new_id) {
-    new_id = new_id.toLowerCase();
-    
-    new_id = new_id.replace(/[^a-z0-9-_.]/g, "");
-    
-    new_id = new_id.replace(/\.+/g,".");
-    
-    if(new_id[0] === ".") new_id = new_id.substring(1);
-    if(new_id[new_id.length-1] === ".") new_id = new_id.slice(0,-1);
-    
-    if(new_id === "") new_id = "a";
-    
-    if(new_id.length >= 16) new_id = new_id.substring(0,15);
-    if(new_id[new_id.length-1] === ".") new_id = new_id.slice(0, -1);
-    
-    while(new_id.length < 3){
-        new_id += new_id[new_id.length-1];
-    }
-    
-    return new_id;
+    var answer = '';
+    return answer;
 }
