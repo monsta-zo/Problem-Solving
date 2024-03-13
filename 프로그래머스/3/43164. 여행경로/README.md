c773f4b7847501fd5eb4543ecad5038b173592d0
Jo Jun Seo <monstazo911@gmail.com>
2024-03-13 22:18:32 +0900

    [level 3] Title: 여행경로, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.18 ms
+메모리: 33.5 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 1:53:16
+2024년 03월 13일 22:18:28
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,53 +1,43 @@
-// 매순간에 알파벳이 빠른 걸 선택하는게 좋은 건 아님
-// 그러다보면 다 여행을 못하는 경우가 생김
-// 백트래킹을 해야함
-
 function solution(tickets) {
     const map = new Map();
     
-    tickets.forEach(([start,end])=>{
-        if(map.has(start)){
-            map.get(start).push([end,false]);
+    tickets.forEach(([start, end])=>{
+        if(map.get(start)){
+            map.get(start).push([end,false])
+            map.get(start).sort((a,b)=>{
+                if(a[0] < b[0]) return -1;
+                if(a[0] > b[0]) return 1;
+                return 0;
+            })
         } else {
-            map.set(start,[[end,false]]);
+            map.set(start, [[end, false]]);
         }
     })
-    
+    const routes = ['ICN'];
     let answer = [];
-    let routes = ["ICN"];
-    
-    for(let value of map.values()){
-            value.sort((a,b)=>{
-                if(a[0]>b[0]) return 1;
-                else return -1;
-            });
-    }
-    
-    const isAll = () => {
-        return Array.from(map.values()).every(start=>start.every(ticket=>ticket[1]===true));
-    }
-    
-    const dfs = () => {
-        if(isAll()){
-            answer.push([...routes]);
+
+    const dfs = (cur) => {
+        if(routes.length === tickets.length+1){
+            if(answer.length === 0) answer = [...routes];
             return;
         }
-        const nexts = map.get(routes[routes.length-1]);
-        if(!nexts) return;
-        nexts.forEach(next=>{
-            if(!next[1]){
-                routes.push(next[0]);
-                next[1] = true;
-                dfs();
-                routes.pop();
-                next[1] = false;
+        
+        const next = map.get(cur);
+        if(next){
+              for(let i=0; i<next.length; i++){
+                if(!next[i][1]){
+                    routes.push(next[i][0]);
+                    next[i][1] = true;
+                    dfs(next[i][0])
+                    routes.pop();
+                    next[i][1] = false;
+                }
             }
-            
-        })
+        }
+      
     }
     
-    dfs();
+    dfs("ICN");
     
-    return answer[0];
-    
-}
+    return answer;
+ }
