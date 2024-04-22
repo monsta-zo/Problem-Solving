10e3ac280395c0df8f229f65708671c05971cc95
Jo Jun Seo <monstazo911@gmail.com>
2024-04-22 11:25:35 +0900

    [Bronze V] Title: A+B - 4, Time: 136 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,9 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+
+let idx = 0;
+
+while(input[idx]){
+  const [A, B] = input[idx].split(" ").map(Number);
+  console.log(A+B);
+  idx++;
+}
