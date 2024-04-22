d24d0d17d27cdde5e94fc8b52e8a0b55dff00a75
Jo Jun Seo <monstazo911@gmail.com>
2024-04-22 11:17:31 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,7 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+
+console.log("|\\_/|")
+console.log("|q p|   /}")
+console.log("( 0 )\"\"\"\\")
+console.log("|\"\^\"\`    |")
+console.log("||_/=\\\\__|")
