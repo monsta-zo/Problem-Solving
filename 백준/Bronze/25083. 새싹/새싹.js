8544a2b54fa17b1a7a8e6c2202c98628d0ee4eae
Jo Jun Seo <monstazo911@gmail.com>
2024-04-22 11:41:38 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,8 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
