c7fbe6bc6661da0fa34baed7463747d20fad1981
Jo Jun Seo <monstazo911@gmail.com>
2024-04-23 11:42:35 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 9332 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -4,4 +4,4 @@ const str = input[1];
 
 const arr = str.split("").map((c)=>c.charCodeAt()-96);
 
-console.log(arr.reduce((acc,cur,idx)=> acc+cur*(31**idx) ,0) % 1234567891);
+console.log((arr.reduce((acc,cur,idx)=> acc+BigInt(cur)*(31n**BigInt(idx)) ,0n) % 1234567891n).toString());
